194f8c14738512f36c0cff2bf5da5003aa627bdf
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-02-22 00:50:19 -0500

    verbose names

--- src/users/models.py
+++ src/users/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField("The name of the person",max_length=30)
+    last_name = models.CharField("The last name of the person",max_length=30)
+    cars = models.ManyutoManyField('Car', verbose_name="cars owned by the user")
 
 STATUS_CHOICES = (
     ('R', 'Reviewed'),
@@ -14,7 +15,7 @@ STATUS_CHOICES = (
 
 class Website(models.Model):
     name = models.CharField(max_length=30)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     release_date = models.DateField()
     rating = models.IntegerField()
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
