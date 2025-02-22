98dcac3f1efba5c4c6a513f6bc03311ffad3b2b4
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-02-21 21:54:12 -0500

    DjangoBlog part 1: creating models

---
+++ src/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+STATUS_CHOICES = (
+    ('R', 'Reviewed'),
+    ('N', 'not reviewed'),
+    ('E', 'error'),
+    ('A', 'accepted'),
+)
+
+class Website(models.Model):
+    name = models.CharField(max_length=30)
+    url = models.URLField()
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
