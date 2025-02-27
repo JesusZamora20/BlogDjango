4971c809bf36c087f6eb0381e3aa2c229d51cb9e
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-02-26 23:34:53 -0500

    Namespaces

--- URLs/URLs/urls.py
+++ URLs/URLs/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from posts.views import dummy_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dummy_view, name='home'),
     path('entries',include('posts.urls',namespace='entries'))
-
 ]
--- URLs/posts/urls.py
+++ URLs/posts/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import dummy_view
 
 urlpatterns = [
     path('', dummy_view, name='entry-list'),
-    path('<id>/', dummy_view, name='entry-detail'),
+    path('<int:id>/', dummy_view, name='entry-detail'),
+    path('(?P<id>[0-9]{4}/(?P<slug>[\w-]+)/$)', dummy_view, name='entry-detail'),
     path('<id>/delete/', dummy_view, name='entry-delete'),
     path('<id>/update/', dummy_view, name='entry-update'),
 ]
