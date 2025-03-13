99db2ef27135b3f7d1c3b7a9d1df2abad75d5eb0
jesu <jbermudezamora87@gmail.com>
2025-03-12 23:21:56 -0500

    Creating models

---
+++ blog/posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post, Comment, PostView, Like
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(PostView)
+admin.site.register(Like)
+
