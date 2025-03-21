951606fc7a6a35e70df9650c04739df6f6e5795f
jesu <jbermudezamora87@gmail.com>
2025-03-21 00:08:43 -0500

    finishing the views

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -10,16 +10,24 @@ class PostDetailView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
+    fields = (
+        "title",
+        "content",
+        "thumbnail",
+        "author",
+        "slug",
+    )
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = (
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug'
+        "title",
+        "content",
+        "thumbnail",
+        "author",
+        "slug",
     )
 
 class PostDeleteView(DeleteView):
     model = Post
+    success_url = "/"
